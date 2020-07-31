ac3cd52ec1b76a028c9356c4cdfc04a7ddccdf7b
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-07-31 11:29:05 +0600

    Added necessary dependencies for heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,8 +84,7 @@ TEMPLATES = [
                 'coresite.context_processors.categories_context_processor',
 
                 # 3rd Party
-		        # 'whitenoise.middleware.WhiteNoiseMiddleware',
-                # 'django.core.context_processors.request',
+                'whitenoise.middleware.WhiteNoiseMiddleware',
             ],
         },
 
@@ -182,9 +181,8 @@ MESSAGE_TAGS = {
 
 
 
-# # Activate Django-Heroku.
-# django_heroku.settings(locals())
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
