5e90c3dcd05ce7ecffd768a51df407549996ecf9
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-07-31 14:52:38 +0600

    Debug was false

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')ce3afwgsfm@2*4to89yxs&=$nqxcz(cujim3$cm6$#*z2v)gb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['gainskill.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
