78e731f913052b3743a8511f956e7ccdf89d5448
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-08-05 17:13:48 +0600

    updated

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')ce3afwgsfm@2*4to89yxs&=$nqxcz(cujim3$cm6$#*z2v)gb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['gainskill.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['gainskill.herokuapp.com',]
 
 
 # Application definition
