60621996417b5c7026126843c21465844d504025
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2021-02-01 23:59:27 +0600

    updated

---
+++ courses/templatetags/is_category_has_course.py
@@ -0,0 +1,14 @@
+from django import template
+
+from courses.models import Course
+
+register = template.Library()
+
+
+@register.simple_tag(name='is_category_has_course')
+def is_category_has_course(category):
+    has_course = Course.objects.filter(category=category)
+    if has_course:
+        return True
+    else:
+        return False
